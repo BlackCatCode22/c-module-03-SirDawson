89cfd1a9fa6582bb4b0dcfc227bf66c66bebdf11
SirDawson <siraaramisdawson@gmail.com>
2024-09-11 12:36:53 -0700

    Add files via upload
    Committed another assignment.

---
+++ pointersArePowerful.cpp
@@ -0,0 +1,38 @@
+/*
+sD 9/11/24
+pointersArePowerful.cpp
+An array is a fixed-sized data structure that must contain the same data types.
+*/
+
+#include <iostream>
+
+using std::cout, std::string;
+
+int main()
+{   
+    int a = 22;
+    int* p;
+
+    p = &a;
+
+    cout << "The memory address of 'p' is: " << p << ".\n"
+         << "The value of 'a' is: " << *p << ".\n";
+
+    *p = 25;
+
+    cout << "The modified version of 'a' is: " << a << ".\n\n"
+         << "----------------------------------------------\n\n";
+
+    int arr[5];
+
+    for(int a = 0; a < 5; a++)
+    {
+       cout << "The memory address of 'arr[" << a << "]' is: " << &arr[a] << ".\n";
+    }
+
+    cout << "\n" << "----------------------------------------------\n\n"
+         << "End of program.";
+
+    system("pause>0");
+    return 0;
+}
